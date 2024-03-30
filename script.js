db4cce980475eb7b11f12a7c3d058770bc328987
Julius Huhtasalo <122513122+JuliusHuhtasaloReal@users.noreply.github.com>
2024-03-31 00:34:50 +0200

    Yeaaa

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeBackground() {
+    const images = [
+        '/images/chinesesunshine.jpeg',
+        '/images/darksunshine.jpeg',
+        '/images/lebronsunshinecover.jpg'
+    ];
+
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+
+    document.body.style.backgroundImage = `url('${selectedImage}')`;
+    document.getElementById('nappi').style.display = 'none';
+  
+    const audio = document.getElementById('audio');
+    audio.play();
+}
