428910f095e1290c88c7b8d27de111380cfa6a33
Julius Huhtasalo <122513122+JuliusHuhtasaloReal@users.noreply.github.com>
2024-03-31 00:55:11 +0200

    updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function changeBackground() {
     const images = [
-        '/images/chinesesunshine.jpeg',
-        '/images/darksunshine.jpeg',
-        '/images/lebronsunshinecover.jpg'
+        'images/chinesesunshine.jpeg',
+        'images/darksunshine.jpeg',
+        'images/lebronsunshinecover.jpg'
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
