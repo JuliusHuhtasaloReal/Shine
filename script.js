941c216f64bddb903e4d7f75345b7080cd19fa5f
Julius Huhtasalo <122513122+JuliusHuhtasaloReal@users.noreply.github.com>
2024-03-31 18:29:43 +0300

    Depsi sunshine

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ function changeBackground() {
     document.body.style.backgroundColor = 'black';
     document.getElementById('nappi').style.display = 'none';
     document.body.classList.toggle('zoomed');
-  
-    const audio = document.getElementById('audio');
-    audio.play();
+    if(randomIndex == 3){
+        const depsi_audio = document.getElementById('depsi_audio');
+        depsi_audio.play();
+    }
+    else{
+        const audio = document.getElementById('audio');
+        audio.play();
+    }
+    
 }
