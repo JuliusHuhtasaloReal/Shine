83ff0f6818986ee14510caafcab9be975b8dd48e
Julius Huhtasalo <122513122+JuliusHuhtasaloReal@users.noreply.github.com>
2024-03-31 02:08:08 +0200

    URMASunShain

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ function changeBackground() {
     const images = [
         'images/chinesesunshine.jpeg',
         'images/darksunshine.jpeg',
-        'images/lebronsunshinecover.jpg'
+        'images/lebronsunshinecover.jpg',
+        'images/depsishine.png',
+        'images/julle.png'
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
     const selectedImage = images[randomIndex];
 
     document.body.style.backgroundImage = `url('${selectedImage}')`;
+    document.body.style.backgroundColor = 'black';
     document.getElementById('nappi').style.display = 'none';
+    document.body.classList.toggle('zoomed');
   
     const audio = document.getElementById('audio');
     audio.play();
